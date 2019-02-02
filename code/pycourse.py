7e7bd82456f2461ba76c5d6fb24268c0e55637d0
gripepi <gian.ripepi96@gmail.com>
2019-02-02 09:59:05 +0000

    Adding basic python datatypes

---
+++ code/pycourse.py
@@ -0,0 +1,16 @@
+# N.B. We will be using python 3
+
+integer = 7
+type(integer) # <class 'int'>
+
+floating_number = 3.14
+type(floating_number) # <class 'float'>
+
+string1 = "I am a string"
+type(string1) # <class 'str'>
+
+string2 = 'I am a string too'
+type(string2) # <class 'str'>
+
+char = 'g'
+type(char) # <class 'str'>
