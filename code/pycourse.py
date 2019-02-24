9505d6288783951e9d496453605aa3ae2876441d
gripepi <gian.ripepi96@gmail.com>
2019-02-24 13:13:30 +0000

    Adding functools

--- code/pycourse.py
+++ code/pycourse.py
@@ -242,7 +242,7 @@ while x < 10:
 	print(x, "is less than 10")
 	x += 1
 
-# for and in loop
+# for and in loop - range is exclusive and can also take 2 arguments (start, end)
 for x in range(10):
 	print(x, "is less than 10")
 
@@ -253,3 +253,61 @@ for x in range(10):
 	if x == 5:
 		break # quit the loop entirely
 	print(x) # 1, 2, 4
+
+# MODULES AND FUNCTOOLS
+
+# to use modules use keyword import
+import functools
+
+# modules can be given an alias using keyword as
+import functools as first_three
+
+# can also import only the part of a module using keyword from
+from functools import partial, reduce
+
+def exp(base, power):
+	return base**power # want to use this function to create two_to_the function
+
+# we can use this
+def two_to the(power):
+	return exp(2, power) # this can get unwiedly
+
+two_to_the = partial(exp, 2) # it is a new fuction of one variable
+print(two_to_the(3)) # 8
+# or
+square_of = partial(exp, power=2) # you can specify the argument name and specify the rest later
+print(square_of(3)) # 9
+
+# other functional tools are: map, reduce, filter - which provide alternatives to list comprehensions
+# map
+def double(x):
+	return 2*x
+
+xs = [1, 2, 3, 4]
+twice_xs = [double(x) for x in xs] # [2, 4, 6, 8]
+twice_xs = map(double, xs) # same as above
+list_doubler = partial(map, double) # *function* that doubles a list
+twice_xs = list_doubler(xs) # again [2, 4, 6, 8]
+
+# map can have multiple arguments if you provide multiple lists
+def multiply(x, y):
+	return x*y
+
+products = map(multiply, [1, 2], [4, 5]) # [1*4, 2*5] = [4, 10]
+
+# filter does the work of list-comprehensions if
+def is_even(x):
+	return x%2 == 0
+
+x_evens = [x for x in xs if is_even(x)] # [2, 4]
+x_evens = filter(is_even, xs) # same as above
+list_evener = partial(filter, is_even) # *function* that filters a list
+x_evens = list_evener(xs) # again [2, 4]
+
+# reduce combines the first two elements of a list, then the result with the third,
+# that result with the fourth, and so on, producing a single result 
+# from functools import partial, reduce - has to be imported from functools
+
+x_product = reduce(multiply, xs) # = 1*2*3*4 = 24
+list_product = partial(reduce, multiply) # *function* that reduces a list
+x_product = list_product(xs) # again = 24
